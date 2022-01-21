83be2be696e0ab9d95d031c0f3d5360f0f0677c6
SeongMin-K <eo1tks3@naver.com>
2022-01-21 13:29:11 +0900

    Completed

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctBtn: UIButton!
+    @IBOutlet weak var tenPctBtn: UIButton!
+    @IBOutlet weak var twentyPctBtn: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    var finalResult = "0.0"
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        zeroPctBtn.isSelected = false
+        tenPctBtn.isSelected = false
+        twentyPctBtn.isSelected = false
+        sender.isSelected = true
+        
+        let btnNumber = String(sender.currentTitle!.dropLast())
+        tip = Double(btnNumber)! / 100
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = Int(sender.value).description
+        numberOfPeople = Int(sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let bill = billTextField.text!
+        if bill != "" {
+            billTotal = Double(bill)!
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            finalResult = String(format: "%.2f", result)
+            
+            performSegue(withIdentifier: "goToResults", sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResults" {
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.result = finalResult
+            destinationVC.tip = Int(tip * 100)
+            destinationVC.split = numberOfPeople
+        }
+    }
+}
+
